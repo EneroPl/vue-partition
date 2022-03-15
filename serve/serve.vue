be8340aa8908d086232971b7dc39e6fd2d278835
e.lankovich <e.lankovich@ddos-guard.net>
2022-03-15 15:36:48 +0300

    Initialize local server for testing

---
+++ serve/serve.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <VuePartition v-model="options" :total="total" />
+  </div>
+</template>
+
+<script>
+import VuePartition from './../src/components/VuePartition.vue';
+
+export default {
+  components: { VuePartition },
+  data() {
+    return {
+      options: [
+        {
+          value: 1000,
+          settings: {
+            background: '#999999'
+          }
+        }
+      ],
+      total: 1000
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
